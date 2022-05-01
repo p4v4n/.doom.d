0d050b0ff2fca4ef0b56f2255fcebdd8c99d03e5
p4v4n <pavan.mantha99@gmail.com>
2022-05-02 00:18:57 +0530

    Setup secrets

--- config.el
+++ config.el
@@ -3,11 +3,13 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+(when (file-exists-p "secrets/auth.el")
+  (load! "secrets/auth"))
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -89,3 +91,7 @@
 
 ;; load all modules
 (load! "+modules")
+
+;; secrets
+(when (file-exists-p "secrets/config.el")
+  (load! "secrets/config"))
