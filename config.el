d7d4a87d30a4a4a75db55ebcb1a9580c194a8090
p4v4n <pavan.mantha99@gmail.com>
2022-05-01 19:56:49 +0530

    Toggle fullscreen on load

--- config.el
+++ config.el
@@ -85,5 +85,7 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+(add-hook 'window-setup-hook #'toggle-frame-fullscreen)
+
 ;; load all modules
 (load! "+modules")
